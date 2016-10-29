e52ad21fa9f36de9e71c2957a7a8060e48fa6853
guilherme2020 <guilhermerodrigues123477@gmail.com>
2016-10-28 22:17:02 -0200

    ajustes

--- BancoClientes.php
+++ BancoClientes.php
@@ -13,17 +13,17 @@ class BancoDeClientes{
 
 		self::$clientes = array(
 
-			0 => new Cliente('Guilherme Rodrigues Simeao','06007517322','Rua das Palmeiras n°2290 Colorado');
-			1 => new Cliente('Edvaldo  Simeao','44407517322','Rua das Palmeiras n°2290 Colorado')
-			2 => new Cliente('Victoria Simeao','2133131322','Rua das Palmeiras n°2290 Colorado');
-			3 => new Cliente('Dhara Mendes Custodio Simeao','12123117322','Rua das Palmeiras n°2290 Colorado');
-			4 => new Cliente('Breno do Monte Machado ','212121217322','Rua das Palmeiras n°2290 Colorado');
-			5 => new Cliente('Anderson Miranda','1220112','centro');
-			6 => new Cliente('Bartolomeu Cesar ','1122112','DF');
-			7 => new Cliente('Pedro Alex','122121','PARQUE Centro');
-			8 => new Cliente('Victor Hugor','2212121','Morada Nova');
-			9 => new Cliente('Lucas Miranda','21221212112','Morada Nova');
-			10 => new Clinete('Arcenio Pereira José','Lourival Parente');
+			0 => new Cliente('Guilherme Rodrigues Simeao','06007517322','Rua das Palmeiras n°2290 Colorado'),
+			1 => new Cliente('Edvaldo  Simeao','44407517322','Rua das Palmeiras n°2290 Colorado'),
+			2 => new Cliente('Victoria Simeao','2133131322','Rua das Palmeiras n°2290 Colorado'),
+			3 => new Cliente('Dhara Mendes Custodio Simeao','12123117322','Rua das Palmeiras n°2290 Colorado'),
+			4 => new Cliente('Breno do Monte Machado ','212121217322','Rua das Palmeiras n°2290 Colorado'),
+			5 => new Cliente('Anderson Miranda','1220112','centro'),
+			6 => new Cliente('Bartolomeu Cesar ','1122112','DF'),
+			7 => new Cliente('Pedro Alex','122121','PARQUE Centro'),
+			8 => new Cliente('Victor Hugor','2212121','Morada Nova'),
+			9 => new Cliente('Lucas Miranda','21221212112','Morada Nova'),
+			10 => new Cliente('Arcenio Pereira José','Lourival Parente')
 
 		);
 	}
@@ -31,7 +31,7 @@ class BancoDeClientes{
 
 
 	public function getClientes($order = "ASC"){
-		if($orden == "DESC"){
+		if($order == "DESC"){
 			rsort(self::$clientes);
 			return self::$clientes;
 		}
